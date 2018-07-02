9bdcd2868522d0c540f48148873b8b2702498d9a
jadahuang <499407125@qq.com>
2018-07-02 18:58:36 +0800

    可以组成的回文字符串的最大长度 "abccccdd"  -> "dccaccd"  7

--- src/String/409_longest-palindrome_easy.py
+++ src/String/409_longest-palindrome_easy.py
@@ -1,9 +1,6 @@
 # coding:utf-8
 # 计算一组字符集合可以组成的回文字符串的最大长度
 
-# Time:  O(n)
-# Space: O(1)
-
 # Given a string which consists of lowercase or uppercase letters,
 # find the length of the longest palindromes that can be built with those letters.
 #
@@ -22,10 +19,37 @@
 # 使用长度为 256 的整型数组来统计每个字符出现的个数，每个字符有偶数个可以用来构成回文字符串。
 # 因为回文字符串最中间的那个字符可以单独出现，所以如果有单独的字符就把它放到最中间。
 
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        slen = len(s)
+        count = {}    # 记录每个字符出现次数
+        for c in s:
+            if c in count:
+                count[c] += 1
+            else:
+                count[c] = 1
+        odds = 0
+        for i in count:
+            if count[i] % 2 != 0:  # 记录出现奇数次字符个数
+                odds += 1
+
+        if odds == 0:         # 如果没有奇数次字符,则整个字符串都可以组成回文
+            return slen
+        else:
+            return (slen-odds)+1   # 如果有奇数次字符, 减去这些字符, 再加上一个奇数次字符(放在中间)
+
+
 import collections
 
 
-class Solution(object):
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
     def longestPalindrome(self, s):
         """
         :type s: str
@@ -45,25 +69,4 @@ class Solution(object):
         return len(s) - odd + int(odd > 0)
 
 
-class Solution2(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        slen = len(s)
-        count = {}
-        for c in s:
-            if c in count:
-                count[c] += 1
-            else:
-                count[c] = 1
-        odds = 0
-        for i in count:
-            if count[i] % 2 != 0:
-                odds += 1
 
-        if odds==0:
-            return slen
-        else:
-            return (slen-odds)+1
