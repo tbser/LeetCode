5073a8a375bfb0349d4e95673bbb5aa31996a177
jadahuang <499407125@qq.com>
2018-07-04 08:24:38 +0800

    Bubble sort  最大的数往后冒

--- src/Sort/O(n^2)_冒泡排序.py
+++ src/Sort/O(n^2)_冒泡排序.py
@@ -1,25 +1,24 @@
 # Time: O(n^2)
 
-# 0 - N-1 从第一个数往后依次比较,大的往后移, 最大的数到达最后一个位置
-# 0 - N-2 第二大的数到达倒数第二个位置
+# 0 - N-1 选出一个最小值,放到第一个位置 (所有的数中选一个最小值)
+# 1 - N-1 选出一个最小值,放到第二个位置 (除了第一个数, 所有后面的数中选一个最小值)
 # ...
 
 
-class Solution(object):
-    def bubbleSort(self, A):
-        n = len(A)
-        flag = 0
-        while n:
-            for i in range(n-1):   # 对前l-1个数遍历(除了最后一个数 前面的数)
-                if A[i] > A[i+1]:  # 当前数和后面一个数比较, 大的话交换位置
-                    temp = A[i]
-                    A[i] = A[i+1]
-                    A[i+1] = temp
-                    flag = 1
-            if not flag:   # 没有交换过程,已经有序,直接结束循环
-                break
-            n -= 1
-        return A
+def bubbleSort(self, A):
+    n = len(A)
+    flag = 0
+    while n:
+        for i in range(n - 1):  # 与后l-1个数比较
+            if A[i] > A[i + 1]:  # 当前数和后面一个数比较, 大的话交换位置
+                temp = A[i]
+                A[i] = A[i + 1]
+                A[i + 1] = temp
+                flag = 1
+        if not flag:  # 没有交换过程,已经有序,直接结束循环
+            break
+        n -= 1
+    return A
 
 
 if __name__ == '__main__':
